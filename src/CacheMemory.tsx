9a5f38e586cdb48f0175d933d4cf1f9513c41677
Jozias Martini <jozias.martini@gmail.com>
2022-10-11 18:39:50 -0300

    Change functions to react type functions

--- src/CacheMemory.tsx
+++ src/CacheMemory.tsx
@@ -1,47 +1,38 @@
 import './CacheMemory.sass';
-
-const randomBit = (count: number) => {
-  let number = "";
-  for (let i = 0; i < count; i++) {
-    number += Math.round(Math.random());
-  }
-  return number;
-};
-
-let cacheInitial = randomBit(4);
-let memoryInitial = randomBit(8);
-let hitInitial = randomBit(1);
-
-//Option read
-// function optionRead() {
-//   let position = input.value;
-//   let valueInPosition = memoryVector[position];
-//   option.innerHTML = "O valor na posição " + position + " é " + valueInPosition + ".";
-// }
-
-//Option write
-// function optionWrite() {
-//   let inputData = input.value;
-//   let valueFinded = false;
-//   for (i = 0; i < memoryVector.length; i++) {
-//     if (inputData == memoryVector[i]) {
-//       valueFinded = true;
-//       option.innerHTML = "The value " + inputData + " is in the position [" + i + "]";
-//     }
-//     if (valueFinded == false) {
-//       option.innerHTML = "The value " + inputData + " is not in the memory and will be added";
-//       memoryVector.pop();
-//       memoryVector.push(inputData);
-//       displayDataOnScreen();
-//     }
-//   }
-// }
+import { useEffect } from 'react';
 
 function CacheMemory() {
+  let cacheInitial: number[] = [];
+  let memoryInitial: number[] = [];
+  let hitInitial: number[] = [];
+
+  const randomBit = (count: number) => {
+    let number = 0;
+    for (let i = 0; i < count; i++) {
+      number += Math.floor(Math.random() * 1) + 1;
+    }
+    return number;
+  };
+  
+  const initMemory = () => {
+    let counter: number = 0;
+  
+    while (counter < 8) {
+      cacheInitial.push(randomBit(4));
+      memoryInitial.push(randomBit(8));
+      hitInitial.push(randomBit(1));
+      counter++;
+    }
+  }
+
+  useEffect(() => {
+    initMemory();
+  })
+
   return (
     <main className="cache-memory-container">
       <header className="cache-memory-header">
-        <svg className="cpu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="60" height="60"><path fill="#333333" fill-rule="evenodd" d="M8.75 8a.75.75 0 00-.75.75v6.5c0 .414.336.75.75.75h6.5a.75.75 0 00.75-.75v-6.5a.75.75 0 00-.75-.75h-6.5zm.75 6.5v-5h5v5h-5z"></path><path fill="#333333" fill-rule="evenodd" d="M15.25 1a.75.75 0 01.75.75V4h2.25c.966 0 1.75.784 1.75 1.75V8h2.25a.75.75 0 010 1.5H20v5h2.25a.75.75 0 010 1.5H20v2.25A1.75 1.75 0 0118.25 20H16v2.25a.75.75 0 01-1.5 0V20h-5v2.25a.75.75 0 01-1.5 0V20H5.75A1.75 1.75 0 014 18.25V16H1.75a.75.75 0 010-1.5H4v-5H1.75a.75.75 0 010-1.5H4V5.75C4 4.784 4.784 4 5.75 4H8V1.75a.75.75 0 011.5 0V4h5V1.75a.75.75 0 01.75-.75zm3 17.5a.25.25 0 00.25-.25V5.75a.25.25 0 00-.25-.25H5.75a.25.25 0 00-.25.25v12.5c0 .138.112.25.25.25h12.5z"></path></svg>
+        <svg className="cpu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="60" height="60"><path fill="#333333" fillRule="evenodd" d="M8.75 8a.75.75 0 00-.75.75v6.5c0 .414.336.75.75.75h6.5a.75.75 0 00.75-.75v-6.5a.75.75 0 00-.75-.75h-6.5zm.75 6.5v-5h5v5h-5z"></path><path fill="#333333" fillRule="evenodd" d="M15.25 1a.75.75 0 01.75.75V4h2.25c.966 0 1.75.784 1.75 1.75V8h2.25a.75.75 0 010 1.5H20v5h2.25a.75.75 0 010 1.5H20v2.25A1.75 1.75 0 0118.25 20H16v2.25a.75.75 0 01-1.5 0V20h-5v2.25a.75.75 0 01-1.5 0V20H5.75A1.75 1.75 0 014 18.25V16H1.75a.75.75 0 010-1.5H4v-5H1.75a.75.75 0 010-1.5H4V5.75C4 4.784 4.784 4 5.75 4H8V1.75a.75.75 0 011.5 0V4h5V1.75a.75.75 0 01.75-.75zm3 17.5a.25.25 0 00.25-.25V5.75a.25.25 0 00-.25-.25H5.75a.25.25 0 00-.25.25v12.5c0 .138.112.25.25.25h12.5z"></path></svg>
         <h1 className="cache-memory-header__title">cache-memory</h1>
         <p className="cache-memory-header__subtitle">A cache memory implementation in TypeScript.</p>
       </header>
@@ -51,7 +42,8 @@ function CacheMemory() {
           <section className="cache-memory-button-area">
             <span onClick={() => {}} className="cache-memory-button">Read</span>
             <span onClick={() => {}} className="cache-memory-button">Write</span>
-            <span onClick={() => {}} className="cache-memory-button">List</span>
+            <span onClick={() => {}} className="cache-memory-button">Clean</span>
+            <span onClick={() => {}} className="cache-memory-button">Process</span>
           </section>
         </section>
 
